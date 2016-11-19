61f5928438983351091d22e96dd8fa19d3967fd5
guest-g2iyme <guest-g2iyme@istc-lab>
2016-11-19 17:24:28 +0400

    brackets problem

---
+++ grammar-and-types/brackets.js
@@ -0,0 +1,31 @@
+
+
+/**
+* Problem instructions: how can we solve the problem1?
+* Relevant resources: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Gram$
+*/
+function problem1(){
+  console.log c
+  let r=35
+  let p=19
+  let c=r+p
+}
+
+/**
+ * Solution - below is a base64 blob of the solution.
+ *
+--begin--
+ZnVuY3Rpb24gcHJvYmxlbTEoKXsNCiAgY29uc29sZS5sb2cgKGMpDQogIGxldCByPTM1DQogIGxldCBwPTE5DQog$
+--end--
+ *
+ */
+
+
+
+
+
+
+
+
+
+
