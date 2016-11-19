d035fc83b649d25137a01381e6be5eec0423496f
guest-g2iyme <guest-g2iyme@istc-lab>
2016-11-19 17:11:38 +0400

    cod fix

---
+++ code.js
@@ -0,0 +1,20 @@
+/**
+* Problem instructions: how can we solve the problem1?
+* Relevant resources: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Grammar_and_types
+*/
+function problem1(){
+  console.log c
+  let r=35
+  let p=19
+  let c=r+p
+}
+
+/**
+ * Solution - below is a base64 blob of the solution.
+ *
+--begin--
+ZnVuY3Rpb24gcHJvYmxlbTEoKXsNCiAgY29uc29sZS5sb2cgKGMpDQogIGxldCByPTM1DQogIGxldCBwPTE5DQogIGxldCBjPXIrcA0KfQ0K
+--end--
+ *
+ */
+
